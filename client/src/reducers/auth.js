058434c94949619c750002cbae261313ae0b0da3
DESKTOP-80H0DUI\NpDen4ik <NpDen4ik@yandex.ru>
2019-11-11 08:23:40 +0600

    React-Redux start development

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,15 @@
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL
+} from '../actions/types';
+
+const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null
+}
+
+export default function(state = initialState) {
+
+}
